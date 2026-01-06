dd7c355b934125891ebdcd91b2378e145661b4a8
gaykun1 <139235847+gaykun1@users.noreply.github.com>
2026-01-06 20:56:15 +0200

    feature: global exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,16 +4,25 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // global filter
+  app.useGlobalFilters(
+    new HttpExceptionFilter()
+  );
+
   app.enableCors({
     origin: configService.get<string>('CORS_ORIGIN') || 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+
   await app.listen(configService.get<string>('PORT') ?? 5200, "0.0.0.0");
 
 }
